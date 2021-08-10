fed46c4c7ca9c5fa4b6462ef4360fc1404320e92
jeanboy <eesidoro44@gmail.com>
2021-08-11 00:26:12 +0900

    initialize a3c_ppo.py

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,10 @@
-def list_to_torch():
-    
+import torch
+
+
+def list_to_torch(mini_batch):
+    obs, acts, rewards, next_obs, done = zip(*mini_batch)
+    obs = torch.tensor(obs).float()
+    acts = torch.tensor(acts)
+    rewards = torch.tensor(rewards).float()
+    next_obs = torch.tensor(next_obs).float()
+    done = torch.tensor(done).int()
